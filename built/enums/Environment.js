522b15cdc85355529ef28bfde89bb29661c7dd96
Petr Slepicka <petr.slepicka@radixal.net>
2021-02-11 13:11:57 +0100

    removed built from repo

--- built/enums/Environment.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Environment;
-(function (Environment) {
-    Environment[Environment["DEV"] = 0] = "DEV";
-    Environment[Environment["UAT"] = 1] = "UAT";
-    Environment[Environment["PROD"] = 2] = "PROD";
-})(Environment = exports.Environment || (exports.Environment = {}));
-//# sourceMappingURL=Environment.js.map
