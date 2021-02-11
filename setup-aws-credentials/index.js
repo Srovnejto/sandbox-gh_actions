319567e0b3e6ddcfe4b78d7783c1b7992ecf63b3
Petr Slepicka <petr.slepicka@radixal.net>
2021-02-11 16:48:20 +0100

    returned core.setSecret(AWS_ACCESS_KEY_ID);

--- setup-aws-credentials/index.js
+++ setup-aws-credentials/index.js
@@ -29,15 +29,15 @@ try {
     core.exportVariable('AWS_ENVIRONMENT', AWS_ENVIRONMENT);
 
     console.log("AWS_ACCESS_KEY_ID: " + AWS_ACCESS_KEY_ID);
-    //core.setSecret(AWS_ACCESS_KEY_ID);
+    core.setSecret(AWS_ACCESS_KEY_ID);
     core.exportVariable('AWS_ACCESS_KEY_ID', AWS_ACCESS_KEY_ID);
 
     console.log("AWS_SECRET_ACCESS_KEY: " + AWS_SECRET_ACCESS_KEY);
-    //core.setSecret(AWS_SECRET_ACCESS_KEY);
+    core.setSecret(AWS_SECRET_ACCESS_KEY);
     core.exportVariable('AWS_SECRET_ACCESS_KEY', AWS_SECRET_ACCESS_KEY);
 
     console.log("AWS_REGION: " + AWS_REGION);
-    //core.setSecret(AWS_REGION);
+    core.setSecret(AWS_REGION);
     core.exportVariable('AWS_DEFAULT_REGION', AWS_REGION);
     core.exportVariable('AWS_REGION', AWS_REGION);
 } catch (error) {
