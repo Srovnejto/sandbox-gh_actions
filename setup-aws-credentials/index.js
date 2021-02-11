af905ab1582c91e58bd09b9163a5c031333e6816
Petr Slepicka <petr.slepicka@radixal.net>
2021-02-11 15:08:02 +0100

    updated index.js

--- setup-aws-credentials/index.js
+++ setup-aws-credentials/index.js
@@ -1,12 +1,26 @@
-const core = require("@actions/core")
-const github = require('@actions/github');
+const core = require("@actions/core");
 
 try {
-    const awsEnvironment = core.getInput('aws-environment');
-    console.log("Hello " + awsEnvironment + "!");
+    const awsEnvironment = core.getInput('AWS_ENVIRONMENT');
 
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
+    switch (awsEnvironment) {
+        case "DEVE": {
+            process.env.AWS_ACCESS_KEY_ID = process.env.DEVE_AWS_ACCESS_KEY_ID;
+            process.env.AWS_SECRET_ACCESS_KEY = process.env.DEVE_AWS_SECRET_ACCESS_KEY;
+            break;
+        }
+        case "UAT": {
+            process.env.AWS_ACCESS_KEY_ID = process.env.UAT_AWS_ACCESS_KEY_ID;
+            process.env.AWS_SECRET_ACCESS_KEY = process.env.UAT_AWS_SECRET_ACCESS_KEY;
+            break;
+        }
+        case "PROD": {
+            process.env.AWS_ACCESS_KEY_ID = process.env.PROD_AWS_ACCESS_KEY_ID;
+            process.env.AWS_SECRET_ACCESS_KEY = process.env.PROD_AWS_SECRET_ACCESS_KEY;
+            break;
+        }
+        default: throw "Unknown environment '" + awsEnvironment + "'.";
+    }
 } catch (error) {
     core.setFailed(error.message);
 }
