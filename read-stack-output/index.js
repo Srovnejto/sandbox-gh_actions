58a3f530fb00aab3e0d5b4a47357652761c53b57
Petr Slepicka <petr.slepicka@radixal.net>
2021-02-15 13:43:36 +0100

    Read stack output

---
+++ read-stack-output/index.js
@@ -0,0 +1,14 @@
+const core = require("@actions/core");
+const AWS = require("aws-sdk");
+
+var STACK_NAME = core.getInput('STACK_NAME');
+var cloudFormation = new AWS.CloudFormation();
+cloudFormation.describeStacks({ StackName: STACK_NAME }, (error, data) => {
+    if (error) {
+        console.log(error);
+    } else {
+        data.Stacks[0].Outputs.forEach(output => {
+            core.exportVariable(STACK_NAME + "." + output.OutputKey, output.OutputValue);
+        })
+    }
+});
